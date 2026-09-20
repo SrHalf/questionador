304246ace7d4f9e8a42f32d349393ffb8a98a03d
SrHalf <flavio05@outlook.com>
2026-09-19 21:09:58 -0300

    Adicionado estilo básico de recuo.

--- src/app/(tabs)/questionario.tsx
+++ src/app/(tabs)/questionario.tsx
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 
 export default function Questionario(){
     return (
-        <View>
-            <Text>Aqui vai o questionário selecionado na aba anterior.</Text>
+        <View style={styles.container}>
+            <Text style={{padding: 8, fontSize: 18}}>Aqui vai o questionário selecionado na aba anterior.</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 8,
+  },
+});
