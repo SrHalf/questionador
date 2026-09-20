96e4378531db523bf36fbb639e33cace2af3e32b
SrHalf <flavio05@outlook.com>
2026-09-19 21:05:42 -0300

    Revisado texto da tela sobre.

--- src/app/(tabs)/sobre.tsx
+++ src/app/(tabs)/sobre.tsx
@@ -1,18 +1,18 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Markdown from 'react-native-markdown-display';
 
 const apresentacao = `
 # Questionador
+---
 
-Este é um app simples que apresenta perguntas de verdadeiro ou falso, a partir de arquivos json.
-Este projeto surgiu da necessidade de duas coisas:
+Esta é uma aplicação simples que apresenta perguntas de verdadeiro ou falso, a partir de diversos arquivos JSON internos, e surgiu da necessidade de duas coisas:
 - Aprender React Native.
 - Criar uma solução para estudar e aprender leis diversas (e outros temas).
 
-A ideia de aprender leis vem da minha intenção de **passar em um concurso público**.
-Ainda que eu seja incompetente para isso, espero poder aprender ambos os recursos ao mesmo tempo.
+A ideia de aprender leis vem da minha intenção de **passar em um concurso público**. Ainda que eu seja incompetente para isso, espero poder aprender ambos os recursos ao mesmo tempo.
 
 Já a ideia de fazer uso de enunciados de verdadeiro ou falso surge do concurso atual em que estou matriculado, cuja banca responsável se chama Cebraspe, e que é conhecida por aplicar provas com questões fortemente centradas em verdadeiro ou falso.
+
 Notei, então, que a proposta de reduzir o nível de estudos de um determinado tema em proposições lógicas pode ser muito útil, no sentido de compreendê-lo em cada uma de suas partes mais elementares (algo similar a um método de Descartes), de modo que ao mesmo tempo também faz sentido com os estudos de lógica proposicional, me ajudando, inclusive, nas minhas elucubrações e monólogos sobre o tema.
 
 Em resumo, estou assumindo para mim mesmo que, estudar por um questionário estilo Cebraspe é uma das melhores técnicas de estudo não apenas para este concurso, mas para qualquer ocasião.
@@ -20,8 +20,15 @@ Em resumo, estou assumindo para mim mesmo que, estudar por um questionário esti
 
 export default function Sobre(){
     return (
-        <View>
+        <View style={styles.container}>
             <Markdown>{apresentacao}</Markdown>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 8,
+  },
+});
